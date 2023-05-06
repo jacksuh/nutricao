8995a5ebb4bca33d8c79c3d56d085b75a830ff02
jacksuh <jacksonsdss@gmail.com>
2023-05-06 20:41:16 -0300

    UP: DOC spring

---
+++ src/main/java/com/app/nutricao/infra/springdoc/SpringDocConfigurations.java
@@ -0,0 +1,32 @@
+package com.app.nutricao.infra.springdoc;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfigurations {
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                .info(new Info()
+                        .title("Nutricao")
+                        .description("API Rest Nutricao")
+                        .contact(new Contact()
+                                .name("Jackson Santos")
+                                .email("jacksonsdss@gmail.com"))
+                        .license(new License()
+                                .name("Jackson")
+                                .url("https://jacksuh.github.io/portifolio-page/")));
+    }
+
+}
