bf3a0d5ce45e6eea6ac2d33be93904564fb2eab9
jacksuh <jacksonsdss@gmail.com>
2023-05-11 20:41:35 -0300

    UP: atributos Dto.

--- src/main/java/com/app/nutricao/dto/PacienteDto.java
+++ src/main/java/com/app/nutricao/dto/PacienteDto.java
@@ -1,4 +1,24 @@
 package com.app.nutricao.dto;
 
 public class PacienteDto {
+
+    private String nome;
+
+    private String pesoAtual;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPesoAtual() {
+        return pesoAtual;
+    }
+
+    public void setPesoAtual(String pesoAtual) {
+        this.pesoAtual = pesoAtual;
+    }
 }
