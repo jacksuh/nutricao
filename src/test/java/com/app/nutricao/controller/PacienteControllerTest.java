3d3d54bdfcb1af7bc05a9783a5941c8c209ad431
jacksuh <jacksonsdss@gmail.com>
2023-05-18 16:24:42 -0300

    UP: Teste 201

--- src/test/java/com/app/nutricao/controller/PacienteControllerTest.java
+++ src/test/java/com/app/nutricao/controller/PacienteControllerTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.HttpStatus;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -24,5 +25,22 @@ class PacienteControllerTest {
                 .andReturn().getResponse();
 
         assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    @DisplayName("Test  201")
+    void salvarPaciente() throws Exception{
+
+        String json = "{\"nome\":\"Jackson\",\"pesoAtual\":\"82\"}}";
+
+        var response = mvc.perform(
+                        post("/paciente")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(json)
+                )
+                .andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
+
     }
 }
